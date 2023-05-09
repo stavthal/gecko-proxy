0b2f337708abd81253acf9a93f80ea1579ea287f
stavthal <stavros8003@gmail.com>
2023-05-09 05:29:54 +0300

    single page fixes, home page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import CoinsTable from './components/CoinsTable.jsx';
-import CoinDetail from './components/CoinDetail.jsx';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import CoinInfo from './components/CoinInfo.jsx';
+import HomePage from './components/Home';
+import { BrowserRouter as Router, Route, Switch, withRouter } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={CoinsTable} />
-        <Route path="/coin/:id" component={CoinDetail} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/coins" component={CoinsTable} />
+        <Route exact path="/coin/:id" component={withRouter(CoinInfo)} />
       </Switch>
     </Router>
   );
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import '../css/Home.css';
+
+const HomePage = () => {
+    const [coinList, setCoinList] = useState([]);
+    const [searchInput, setSearchInput] = useState('');
+
+
+    const handleInputChange = (event) => {
+        setSearchInput(event.target.value);
+    }
+
+return (
+    <div className="home">
+      <h1 className="heading">
+        Welcome to our Crypto App!<br/>
+        <span style={{fontSize: '16px', color: "gray"}}>Powered by CoinGecko</span>
+      </h1>
+      <div>
+        <button className="button" onClick={() => window.location.href='/coins'}>View All Coins</button>
+        <div>
+            <div className="or">Or.</div>
+          <label>Search for a single coin:</label><br/>
+          <input className="search-bar" type="text" value={searchInput} onChange={handleInputChange} placeholder="e.g bitcoin, ethereum ... "/><br/>
+          <button className="button" onClick={() => window.location.href=`/coin/${searchInput}`}>Search</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
